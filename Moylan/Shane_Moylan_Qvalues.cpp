a2a53aa56e4eff3717d4f57cbbf1e58323f952d7
Shane <smoylan1@nd.edu>
2015-10-03 06:59:56 -0400

    First Upload

---
+++ Moylan/Shane_Moylan_Qvalues.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+        int Z;
+        int A;
+        cout << "Target Z:\t";
+        cin >> Z;
+        cout << "Target A:\t";
+        cin >> A;
+        
+        cout << "A is " << A << " and Z is " << Z << endl;
+        
+        return 0;
+}
